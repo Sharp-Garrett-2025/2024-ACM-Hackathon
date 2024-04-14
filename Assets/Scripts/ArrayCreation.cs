73e9077bf1d300b98228bfdc5a5e4381ee824952
Contaldi-Quinn-1103 <98000942+Contaldi-Quinn-1104@users.noreply.github.com>
2024-04-13 20:37:06 -0700

    Timer
    Timer and Master Controller is now working. Amazing we have 96% of a game

--- Assets/Scripts/ArrayCreation.cs
+++ Assets/Scripts/ArrayCreation.cs
@@ -9,27 +9,19 @@ using UnityEngine.Assertions.Must;
 public class ArrayCreation : MonoBehaviour
 {
     //we create 
-    int[] numArray;
-    bool[] boolArray;
+    public int[] numArray;
 
     public void Awake()
     {
-        numArray = new int[50];
-        boolArray = new bool[50];
         for (int i = 0; i < numArray.Length; i++) 
         {
             numArray[i] = UnityEngine.Random.Range(0, 3);
-            boolArray[i] = false;
         }
     }
 
     public int[] GetNumArray()
     {
         return numArray;
-    }
-    public bool[] GetBoolArray() 
-    {
-        return boolArray;
     }
     // Update is called once per frame
 }
